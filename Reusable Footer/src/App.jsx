e063e53f8d7822085ce685e2efff9c34c2f3d513
Ankit kumar <ankitjaat00010@gmail.com>
2026-04-29 20:21:51 +0530

    Reusable Footer

---
+++ Reusable Footer/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+export const Footer = () => {
+  return(
+    <footer>
+      <ul>
+        <li>Fitness Dashboard</li>
+        <li>Services</li>
+      </ul>
+      <ul>
+        <li>Watch Videos</li>
+        <li>Discord</li>
+      </ul>
+      <ul>
+        <li>Privacy Policy</li>
+        <li>Terms & Conditions</li>
+      </ul>
+      <p>© 2024 Fitness Dashboard. All Rights Reserved.</p>
+      <a href='#'>🎮</a>
+      <a href='#'>🐦</a>
+      <a href='#'>💻</a>
+      <a href='#'>🏀</a>
+    </footer>
+  )
+}
+
+
+export default Footer
